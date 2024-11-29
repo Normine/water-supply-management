1aefca4cb4af2028eafbfe6d5fb1f9b2ee444ee4
Wang <minhquang1062004@gmail.com>
2024-11-29 20:51:10 +0700

    2

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -3,9 +3,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL = "jdbc:sqlserver://localhost:1433;databaseName=WaterSupplyManagement";
-    private static final String USERNAME = "sa"; // Thay bằng username SQL Server của bạn
-    private static final String PASSWORD = "your_password"; // Thay bằng password của bạn
+    private static final String URL = "jdbc:sqlserver://localhost:3306;databaseName=WaterSupplyManagement";
+    private static final String USERNAME = "root"; // Thay bằng username SQL Server của bạn
+    private static final String PASSWORD = "password"; // Thay bằng password của bạn
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USERNAME, PASSWORD);
