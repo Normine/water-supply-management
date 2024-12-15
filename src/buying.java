aec48d9614d075c30cca27721c7c9ca8203b62d2
ducvu01 <dinhducvu87@gmail.com>
2024-12-15 22:27:18 +0700

    buying - final moderation

--- src/buying.java
+++ src/buying.java
@@ -1,12 +1,16 @@
 
+import java.awt.Font;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.SwingWorker;
+import javax.swing.table.DefaultTableModel;
 
 public class buying extends javax.swing.JFrame {
-
-    public buying() {
+public int id = -1;
+    public buying(int id) {
+        this.id = id;
         initComponents();
+        loadDatatable();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setTitle("Buying products");
         setSize(1000, 768);
@@ -21,11 +25,12 @@ public class buying extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         btnLogout = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
-        tbl = new javax.swing.JTable();
+        jTable1 = new javax.swing.JTable();
         jLabel3 = new javax.swing.JLabel();
-        txtSearch = new javax.swing.JTextField();
-        btnSubmit = new javax.swing.JButton();
+        txtPay = new javax.swing.JTextField();
+        btnPay = new javax.swing.JButton();
         btnRefresh = new javax.swing.JButton();
+        btnBack = new javax.swing.JButton();
 
         btnBuying.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
         btnBuying.setText("Buying");
@@ -52,8 +57,8 @@ public class buying extends javax.swing.JFrame {
             }
         });
 
-        tbl.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
-        tbl.setModel(new javax.swing.table.DefaultTableModel(
+        jTable1.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
                 {null, null, null, null, null, null},
                 {null, null, null, null, null, null},
@@ -66,20 +71,20 @@ public class buying extends javax.swing.JFrame {
                 "ConnectionID", "Name", "Date", "Rating", "Price", "SupplyType"
             }
         ));
-        jScrollPane1.setViewportView(tbl);
+        jScrollPane1.setViewportView(jTable1);
 
         jLabel3.setBackground(new java.awt.Color(255, 255, 255));
         jLabel3.setFont(new java.awt.Font("Arial", 0, 18)); // NOI18N
-        jLabel3.setText("ID of Connection that you want:");
+        jLabel3.setText("Pay for Connection ID:");
         jLabel3.setBorder(javax.swing.BorderFactory.createEtchedBorder());
 
-        txtSearch.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        txtPay.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
 
-        btnSubmit.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
-        btnSubmit.setText("Submit");
-        btnSubmit.addActionListener(new java.awt.event.ActionListener() {
+        btnPay.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        btnPay.setText("Pay");
+        btnPay.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnSubmitActionPerformed(evt);
+                btnPayActionPerformed(evt);
             }
         });
 
@@ -88,6 +93,14 @@ public class buying extends javax.swing.JFrame {
         btnRefresh.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnRefreshActionPerformed(evt);
+            }
+        });
+
+        btnBack.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        btnBack.setText("Back");
+        btnBack.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnBackActionPerformed(evt);
             }
         });
 
@@ -101,21 +114,25 @@ public class buying extends javax.swing.JFrame {
                         .addGap(0, 0, Short.MAX_VALUE)
                         .addComponent(btnRefresh, javax.swing.GroupLayout.PREFERRED_SIZE, 121, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(155, 155, 155)
-                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 441, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(btnLogout, javax.swing.GroupLayout.PREFERRED_SIZE, 121, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addContainerGap(37, Short.MAX_VALUE)
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 947, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addContainerGap()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addComponent(btnBack, javax.swing.GroupLayout.PREFERRED_SIZE, 121, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(28, 28, 28)
+                                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 441, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(btnLogout, javax.swing.GroupLayout.PREFERRED_SIZE, 121, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGap(0, 31, Short.MAX_VALUE)
+                                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 947, javax.swing.GroupLayout.PREFERRED_SIZE)))))
                 .addGap(40, 40, 40))
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(197, 197, 197)
                 .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 267, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(56, 56, 56)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(btnSubmit, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(txtSearch, javax.swing.GroupLayout.PREFERRED_SIZE, 182, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(btnPay, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(txtPay, javax.swing.GroupLayout.PREFERRED_SIZE, 182, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
@@ -124,7 +141,8 @@ public class buying extends javax.swing.JFrame {
                 .addGap(30, 30, 30)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(btnLogout, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnBack, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addComponent(btnRefresh, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(41, 41, 41)
@@ -132,10 +150,10 @@ public class buying extends javax.swing.JFrame {
                 .addGap(32, 32, 32)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(txtSearch, javax.swing.GroupLayout.PREFERRED_SIZE, 26, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(txtPay, javax.swing.GroupLayout.PREFERRED_SIZE, 26, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(32, 32, 32)
-                .addComponent(btnSubmit)
-                .addContainerGap(54, Short.MAX_VALUE))
+                .addComponent(btnPay)
+                .addContainerGap(55, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -158,6 +176,24 @@ public class buying extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    
+    private void loadDatatable(){
+          String[] columnNames = {"Connection ID", "Supplier Name", "Order Date", "Amount", "Price", "Supply Type", "Status"};
+        Object[][] data = DBConnection.getCustomerBuyingList(this.id);
+        
+     
+        // Create DefaultTableModel
+        DefaultTableModel model = new DefaultTableModel(data, columnNames);
+        jTable1.setModel(model);
+        
+        
+          Font smallFont = new Font("Arial", Font.PLAIN, 12); // Change 12 to desired size
+        jTable1.setFont(smallFont);
+
+        // Adjust row height to match the smaller font
+        jTable1.setRowHeight(smallFont.getSize() + 4);
+        
+    }
     private void btnLogoutActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnLogoutActionPerformed
         int option = JOptionPane.showConfirmDialog(
             null,
@@ -191,14 +227,18 @@ public class buying extends javax.swing.JFrame {
                 }
             };
             worker.execute();
+        }
     }//GEN-LAST:event_btnLogoutActionPerformed
-
+    
+    public int getID(){
+    return this.id;
+            }
     private void btnBuyingActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnBuyingActionPerformed
         btnBuying.setEnabled(false);
         SwingWorker<Void, Void> worker =new SwingWorker<>() {
             @Override
             protected Void doInBackground() {
-                buying Buying = new buying();
+                buying Buying = new buying(getID());
                 Buying.setVisible(true);
                 setVisible(false);
                 return null;
@@ -211,29 +251,25 @@ public class buying extends javax.swing.JFrame {
         worker.execute();
     }//GEN-LAST:event_btnBuyingActionPerformed
 
-    private void btnSubmitActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSubmitActionPerformed
+    private void btnPayActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnPayActionPerformed
         btnBuying.setEnabled(false);
-        SwingWorker<Void, Void> worker =new SwingWorker<>() {
-            @Override
-            protected Void doInBackground() {
-                buying Buying = new buying();
-                Buying.setVisible(true);
-                setVisible(false);
-                return null;
-            }
-            @Override
-            protected void done() {
-                btnBuying.setEnabled(true);
-            }
-        };
-        worker.execute();
-    }//GEN-LAST:event_btnSubmitActionPerformed
+           int connectionID = Integer.parseInt(txtPay.getText());
+        DBConnection.Pay(connectionID, getID());
+        loadDatatable();
+    }//GEN-LAST:event_btnPayActionPerformed
 
     private void btnRefreshActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRefreshActionPerformed
         // TODO add your handling code here:
+        loadDatatable();
     }//GEN-LAST:event_btnRefreshActionPerformed
 
-    public static void main(String args[]) {
+    private void btnBackActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnBackActionPerformed
+        // TODO add your handling code here:
+        new customerhome(Integer.toString(this.id)).setVisible(true);
+        dispose();
+    }//GEN-LAST:event_btnBackActionPerformed
+
+    public void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
@@ -260,21 +296,23 @@ public class buying extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new buying().setVisible(true);
+                new buying(getID()).setVisible(true);
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnBack;
     private javax.swing.JButton btnBuying;
     private javax.swing.JButton btnLogout;
+    private javax.swing.JButton btnPay;
     private javax.swing.JButton btnRefresh;
-    private javax.swing.JButton btnSubmit;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTable tbl;
-    private javax.swing.JTextField txtSearch;
+    private javax.swing.JTable jTable1;
+    private javax.swing.JTextField txtPay;
     // End of variables declaration//GEN-END:variables
 }
+
